8a067e4a6fb462d8d2022a683e4ffacbcdbfda85
tongqiu <tongqiu42@gmail.com>
2017-08-10 17:21:09 +0100

    Beginnings of a proper base page

--- blog/static/javascript/base.js
+++ blog/static/javascript/base.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
     $("#menuButton").click(function(){
         if ($("#menu").hasClass("hidden")) {
+            $(this).addClass("change");
             $("#menu").removeClass("hidden");
         } else {
+            $(this).removeClass("change");
             $("#menu").addClass("hidden");
         }
-    }); 
+    });
 });
